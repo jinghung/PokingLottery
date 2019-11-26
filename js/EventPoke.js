a3c7c6d257edaf232f5ec9acea81c2af46f9af6e
jinghung <sa137269@gmail.com>
2019-11-26 16:49:18 +0800

    PokingLottery

---
+++ js/EventPoke.js
@@ -0,0 +1,59 @@
+angular.module('SGT', ['ngSanitize']).controller('PockControll', function ($scope) {
+
+    //戳戳樂-數量
+    $scope.PokeList = new Array(15);
+    for (i = 0; i < $scope.PokeList.length; i++) {
+        $scope.PokeList[i] = i + 1;
+    }
+
+    /*戳戳樂動畫*/
+    $scope.BannerNum = 0;
+    $scope.Rotate = function (i) {
+        $scope.BannerNum++;
+        if ($scope.BannerNum > 12) { //動畫圖片數量
+            return;
+        }
+        $('.PokeItem' + i).find('.poke-status-change').attr('src', 'images/poke/' + $scope.BannerNum + '.png'); 
+        setTimeout(function () {
+            $scope.Rotate(i)
+        }, 100); //圖片切換的時間,1000個單位為一秒鐘
+    }
+
+    $scope.PokeMode = 1; //戳戳樂狀態-0:點擊中,1:沒點。
+
+    //戳戳樂
+    $scope.PokeDay = function (i) { //i:第i個戳戳樂
+        $scope.PokeOut = $('.PokeItem' + i).attr('class');
+        if ($scope.PokeMode == 1 && $scope.PokeOut.indexOf('CursorNon') == -1) {
+            $scope.PokeMode = 0;
+            $('.PokeItem' + i).find('.poke-status-click').show();
+            $.ajax({
+                type: 'GET',
+                url: './js/GetWordCardWeekData.json',
+                async: false,
+                success: function (n) {
+                    $scope.GetAward = n.Data[Math.floor((Math.random() * n.Data.length))];
+
+                    if ($scope.GetAward.ItemID == '10009') {
+                        $scope.GetGiftInfo = '恭喜獲得' + $scope.GetAward.Cnt + ' 老幣。';
+                    } else {
+                        $scope.GetGiftInfo = '恭喜獲得' + $scope.GetAward.ItemName + 'X' + $scope.GetAward.Cnt + '。';
+                    }
+
+                    $scope.Rotate(i);//執行戳戳樂動畫
+                    setTimeout(function () {
+                        $('.PokeItem' + i).find('.poke-status-click').hide();
+                        $('.PokeItem' + i).find('.poke-status-end').show();
+                        $scope.BannerNum = 0;
+                        $scope.PokeMode = 1;
+                        PopComplate($scope.GetGiftInfo, $scope.GetAward.img, i);
+                    }, 1300)
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+            })
+        }
+    }
+
+});
---
+++ js/index.js
@@ -0,0 +1,55 @@
+
+
+$(function () {
+    SizeChange();
+});
+
+$(window).resize(function () {
+    SizeChange();
+});
+
+function SizeChange() {
+    const BodyWidth = $('body').width();
+
+    if (BodyWidth <= 720) {
+        $('#PokeBg').attr('src', 'images/app/poke_bg.jpg');
+    } else {
+        $('#PokeBg').attr('src', 'images/poke_bg.jpg');
+    }
+}
+
+//獲獎POP
+function PopComplate(Info, Img, Pokei) {
+    const PopDIv = '<div id="complate-mask"><div class="complate-wrap"><div class="complate-con"><div class="complate-info"><div class="complate-text color01"></div><div class="complate-giftimg"><img /></div></div><div class="complate-btn"><button><img src="https://static.08online.com/Event/AT/2019/E03_19103101/images/pop_sure.png" class="Ch-Img" /></button></div></div></div></div>';
+    $('body').append(PopDIv);
+    $('#complate-mask .complate-text').html(Info);
+    $('#complate-mask .complate-giftimg>img').attr('src', Img);
+    $('button').click(function () {
+        PopComplateClose(Pokei);
+    })
+    PopSizeChange(688, 434);
+
+}
+
+//獲獎POP-close
+function PopComplateClose(Pokei) {
+    $('#complate-mask').remove();
+    //判斷洞已顯示五個
+    $('.PokeItem' + Pokei).addClass('CursorNon');
+    const ChangeLength = $('.PokeList>.CursorNon').length;
+    if (ChangeLength >= 5) {
+        location.reload();
+    }
+}
+
+//獲獎POP-resize
+function PopSizeChange(boxW, boxH) {
+    const BodyWidth = $('body').width();
+    if (BodyWidth <= 720) {
+        var PopH = ((BodyWidth * boxH) / boxW);
+        $('.complate-wrap').css({
+            'height': PopH,
+            'margin-top': -(PopH / 2)
+        });
+    }
+}
